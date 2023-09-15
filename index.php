21abf1aede0bcecaf80c2ba829e0ba8e9e32a13a
Xtr3me2K <130668153+Xtr3me2K@users.noreply.github.com>
2023-09-15 11:16:20 -0300

    COMMIT_EDITMSG

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Minha Aplicação PHP</title>
+</head>
+<body>
+    <h1>Olá Mundo!</h1>
+    <?php
+    // Seu código PHP pode ser inserido aqui
+    echo "Este é um exemplo de aplicação PHP em execução no Linux.";
+    ?>
+</body>
+</html>
